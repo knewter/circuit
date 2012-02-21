987a9f4b02141df4c289b79666b3e1d8c5204677
Blake Chambers <chambb1@gmail.com>
2012-02-20 21:42:31 -0500

    adds comments for behavior/class_methods

--- lib/circuit/behavior/class_methods.rb
+++ lib/circuit/behavior/class_methods.rb
@@ -4,13 +4,18 @@ module Circuit
   module Behavior
     module ClassMethods
 
-      # creates and memoizes a stack object that will hold the middleware objects
-      # attached to the behavior.
+      # creates and memoizes a stack object that will hold the middleware 
+      # objects attached to the behavior.
       #
+      # @returns [Stack] newly create stack or superclass duplicate
       def stack
-        @stack ||= (superclass and superclass.ancestors.include?(::Circuit::Behavior) ? superclass.stack.dup : Stack.new)
+        @stack ||= (mixing_behaviors? ? superclass.stack.dup : Stack.new)
       end
 
+      # defines accessor methods for commonly used stack configuration 
+      # methods. This would allow you to define behaviors that inherit
+      # stacks form their parents, configure them, without affecting the
+      # parents stack object.
       [:use, :delete, :insert, :insert_after, :insert_before, :swap].each do |methud|
         module_eval <<-METHOD
           def #{methud.to_s}(*args)
@@ -18,6 +23,13 @@ module Circuit
           end
         METHOD
       end
+      
+      private ##############
+      
+        # @private
+        def self_mixes_behaviors?
+          self.superclass and self.superclass.ancestors.include?(::Circuit::Behavior)
+        end
 
     end
   end
